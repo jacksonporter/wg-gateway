ea251724923515fc6565b59d1772a42c7c6e3bc4
Jackson Porter <jackson@jacksonporterjp.com>
2024-02-08 21:19:01 -0700

    wip

--- wg_gateway/api/__init__.py
+++ wg_gateway/api/__init__.py
@@ -1,6 +1,155 @@
 """Package init for the wg-gateway API."""
 
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
 import click
+import uvicorn
+from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+PWD_CONTEXT = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/token")
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+class User(BaseModel):
+    """
+    Defines a user in the wg-gateway system
+    """
+
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+    public_key_pairs: list[str] = []
+
+
+class UserInDB(User):
+    """
+    User information stored in DB
+    """
+
+    hashed_password: str
+
+
+def verify_password(plain_password, hashed_password):
+    return PWD_CONTEXT.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return PWD_CONTEXT.hash(password)
+
+
+def get_user(db, username: str):
+    """
+    Returns user in DB if it exists
+    """
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/api/v1/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
+    """
+    Performs an OAuth2 Login for a user (bearer token)
+    """
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@app.get("/api/v1/whoami")
+async def root(current_user: Annotated[User, Depends(get_current_user)]):
+    """
+    Returns the current user
+    """
+    return current_user
 
 
 @click.group()
@@ -8,3 +157,22 @@ def api():
     """Main entry point for the wg-gateway API."""
     # pylint: disable=unnecessary-pass
     pass
+
+
+@api.command()
+@click.option(
+    "--host",
+    default="0.0.0.0",
+    help="The host to bind to.",
+)
+@click.option(
+    "--port",
+    default=8080,
+    type=int,
+    help="Port to bind to",
+)
+def start(host: str, port: int):
+    """
+    Starts the API
+    """
+    uvicorn.run(app, host=host, port=port)
--- wg_gateway/ui/__init__.py
+++ wg_gateway/ui/__init__.py
@@ -1,10 +1,92 @@
 """Package init for the wg-gateway UI."""
 
+from typing import Optional
+
 import click
+from nicegui import Client, app, ui as nicegui_ui
+from fastapi import Request
+from fastapi.responses import RedirectResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+import httpx
+
+unrestricted_page_routes = {"/login"}
 
 
 @click.group()
 def ui():
-    """Main entry point for the wg-gateway UI."""
+    """Main entry point for the wg-gateway API."""
     # pylint: disable=unnecessary-pass
     pass
+
+
+class AuthMiddleware(BaseHTTPMiddleware):
+    """This middleware restricts access to all NiceGUI pages.
+
+    It redirects the user to the login page if they are not authenticated.
+    """
+
+    async def dispatch(self, request: Request, call_next):
+        if not app.storage.user.get("authenticated", False):
+            if (
+                request.url.path in Client.page_routes.values()
+                and request.url.path not in unrestricted_page_routes
+            ):
+                app.storage.user["referrer_path"] = (
+                    request.url.path
+                )  # remember where the user wanted to go
+                return RedirectResponse("/login")
+        return await call_next(request)
+
+
+app.add_middleware(AuthMiddleware)
+
+
+@nicegui_ui.page("/")
+def main_page() -> None:
+    with nicegui_ui.column().classes("absolute-center items-center"):
+        nicegui_ui.label(f'Hello {app.storage.user["username"]}!').classes("text-2xl")
+        nicegui_ui.button(
+            on_click=lambda: (app.storage.user.clear(), nicegui_ui.open("/login")),
+            icon="logout",
+        ).props("outline round")
+
+
+@nicegui_ui.page("/subpage")
+def test_page() -> None:
+    nicegui_ui.label("This is a sub page.")
+
+
+@nicegui_ui.page("/login")
+def login() -> Optional[RedirectResponse]:
+    def try_login() -> (
+        None
+    ):  # local function to avoid passing username and password as arguments
+        async with httpx.AsyncClient() as client:
+            await client.post("http://localhost:8000")
+
+        
+        if passwords.get(username.value) == password.value:  # this is where I need to talk to the API, and attempt to get a bearer token
+            app.storage.user.update({"bearer_token": token})
+            nicegui_ui.open(
+                app.storage.user.get("referrer_path", "/")
+            )  # go back to where the user wanted to go
+        else:
+            nicegui_ui.notify("Wrong username or password", color="negative")
+
+    if app.storage.user.get("bearer_token"):
+        return RedirectResponse("/")
+    with nicegui_ui.card().classes("absolute-center"):
+        username = nicegui_ui.input("Username").on("keydown.enter", try_login)
+        password = nicegui_ui.input(
+            "Password", password=True, password_toggle_button=True
+        ).on("keydown.enter", try_login)
+        nicegui_ui.button("Log in", on_click=try_login)
+    return None
+
+
+@ui.command()
+def start():
+    """
+    Starts the UI
+    """
+    nicegui_ui.run(storage_secret="THIS_NEEDS_TO_BE_CHANGED", reload=False)
